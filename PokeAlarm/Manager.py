09d995f90c295d99775b9fa03e10d897d5ae5356
fosJoddie <oflatval@gmail.com>
2017-10-17 12:40:15 +0200

    Add a flipping pokedex full of information in the base_stats Generalize raid CP range by calculating it based on pokemon stats Reformat the Utils file so code style is consistent

--- PokeAlarm/Manager.py
+++ PokeAlarm/Manager.py
@@ -15,7 +15,7 @@ from Filters import load_pokemon_section, load_pokestop_section, load_gym_sectio
     load_raid_section
 from Locale import Locale
 from Utils import get_cardinal_dir, get_dist_as_str, get_earth_dist, get_path, get_time_as_str, \
-    require_and_remove_key, parse_boolean, contains_arg
+    require_and_remove_key, parse_boolean, contains_arg, get_pokemon_cp_range
 from Geofence import load_geofence_file
 from LocationServices import LocationService
 
@@ -43,7 +43,6 @@ class Manager(object):
         self.__units = units  # type of unit used for distances
         self.__timezone = timezone  # timezone for time calculations
         self.__time_limit = time_limit  # Minimum time remaining for stops and pokemon
-        self.__cp_ranges = {}
 
         # Set up the Location Specific Stuff
         self.__location = None  # Location should be [lat, lng] (or None for no location)
@@ -73,12 +72,6 @@ class Manager(object):
         # Initialize the queue and start the process
         self.__queue = multiprocessing.Queue()
         self.__process = None
-
-        # Update cp ranges
-        with open(get_path('data/cp_ranges.json'), 'r') as f:
-            cp_ranges = json.loads(f.read())
-            for pkmn_id, value in cp_ranges.iteritems():
-                self.__cp_ranges[int(pkmn_id)] = value
 
         log.info("----------- Manager '{}' successfully created.".format(self.__name))
 
@@ -994,6 +987,8 @@ class Manager(object):
         start_time_str = get_time_as_str(raid['raid_begin'], self.__timezone)
 
         gym_info = self.__gym_info.get(gym_id, {})
+        # Get the catchable cp range for a raid boss of level 20
+        min_cp, max_cp = get_pokemon_cp_range(pkmn_id, 20)
 
         #team id saved in self.__gym_hist when processing gym
         team_id = self.__gym_hist.get(gym_id, '?')
@@ -1016,8 +1011,8 @@ class Manager(object):
             'form': self.__locale.get_form_name(pkmn_id, raid_pkmn['form_id']),
             'team_id': team_id,
             'team_name': self.__locale.get_team_name(team_id)
-            'min_cp': self.__cp_ranges.get(pkmn_id, {}).get('min-cp', '?'),
-            'max_cp': self.__cp_ranges.get(pkmn_id, {}).get('max-cp', '?')
+            'min_cp': min_cp,
+            'max_cp': max_cp
         })
 
         threads = []
